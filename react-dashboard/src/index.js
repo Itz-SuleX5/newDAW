b91b00901f1e47546dcbbd91abb02d79cf0c3f31
Itz-SuleX5 <AA23028@ues.edu.sv>
2025-05-25 10:44:47 -0700

    Initial commit: Auth0 Full Stack Application with Spring Boot and React

---
+++ react-dashboard/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-6a8gx4jqe8ymcodi.us.auth0.com"
+      clientId="LeECmGtmibebqZVG80hUoUUl7ZefIr7a"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
